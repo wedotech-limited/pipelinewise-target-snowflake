d3853739889db14d6879c4d56912c85ebcc6e95f
Ashley Ramdass <ashley@wedotech.co>
2024-06-17 18:16:40 +0400

    feat: timeout parameters

--- target_snowflake/db_sync.py
+++ target_snowflake/db_sync.py
@@ -336,9 +336,7 @@ class DbSync:
                 warehouse=self.connection_config['warehouse'],
                 role=self.connection_config.get('role', None),
                 autocommit=True,
-                login_timeout=60,
-                network_timeout=60,
-                socket_timeout=60,
+                socket_timeout=int(self.connection_config.get("socket_timeout", 300)),
                 session_parameters={
                     # Quoted identifiers should be case sensitive
                     'QUOTED_IDENTIFIERS_IGNORE_CASE': 'FALSE',
@@ -356,9 +354,6 @@ class DbSync:
             warehouse=self.connection_config['warehouse'],
             role=self.connection_config.get('role', None),
             autocommit=True,
-            login_timeout=60,
-            network_timeout=60,
-            socket_timeout=60,
             session_parameters={
                 # Quoted identifiers should be case sensitive
                 'QUOTED_IDENTIFIERS_IGNORE_CASE': 'FALSE',
