977bddf05307b65ffb67cd863c8354c316b6aba4
Ashley Ramdass <ashley@wedotech.co>
2024-07-18 20:12:24 +0400

    fix: flush and close stdout

--- target_snowflake/__init__.py
+++ target_snowflake/__init__.py
@@ -448,6 +448,8 @@ def persist_lines(config,
     emit_state(copy.deepcopy(flushed_state))
     LOGGER.info("Finished persisting all messages")
     LOGGER.info("Target-Snowflake finished")
+    sys.stdout.flush()
+    sys.stdout.close()
 
 
 # pylint: disable=too-many-arguments
